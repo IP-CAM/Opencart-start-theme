af2069b47b1ad567adef925bcbd486263040802a
Daniel Dubchenko <daniel217032001@gmail.com>
2018-07-30 17:38:00 +0300

    feature gulp code structure

--- gulpfile.js
+++ gulpfile.js
@@ -19,12 +19,41 @@ const enabled = {
     failJSHint: argv.production
 };
 
-const THEME_NAME = 'rc-opencart';
+const settings = {
+    theme_name: 'rc-opencart',
+    input: {
+        styles: function () {
+            return [
+                'catalog/view/theme/' + settings.theme_name + '/dev/styles/main.scss',
+                'catalog/view/theme/' + settings.theme_name + '/lib/styles/**/*.scss'
+            ]
+        },
+        scripts: function () {
+            return [
+                'catalog/view/theme/' + settings.theme_name + '/dev/scripts/main.js'
+            ]
+        },
+        reload: function () {
+            return [
+                'catalog/view/theme/' + settings.theme_name + '/template/**/*.tpl',
+                'catalog/view/theme/' + settings.theme_name + '/libs/**/*'
+            ]
+        }
+    },
+    output: {
+        styles: function () {
+            return 'catalog/view/theme/' + settings.theme_name + '/stylesheet'
+        },
+        scripts: function () {
+            return 'catalog/view/theme/' + settings.theme_name + '/scripts'
+        }
+    }
+};
 
 // ### Styles
 // `gulp styles` - Compiles, combines, and optimizes  project CSS.
 gulp.task('styles', function () {
-    return gulp.src('./catalog/view/theme/'+THEME_NAME+'/dev/styles/main.scss')
+    return gulp.src(settings.input.styles())
         .pipe(gulpif(enabled.maps, sourcemaps.init()))
         .pipe(sass({
             includePaths: bourbon.includePaths
@@ -32,7 +61,7 @@ gulp.task('styles', function () {
         .pipe(autoprefixer(['last 15 versions']))
         .pipe(cleanCSS())
         .pipe(gulpif(enabled.maps, sourcemaps.write()))
-        .pipe(gulp.dest('catalog/view/theme/'+THEME_NAME+'/stylesheet'))
+        .pipe(gulp.dest(settings.output.styles()))
         .pipe(browserSync.reload({stream: true}))
 });
 
@@ -42,17 +71,15 @@ gulp.task('watch', function () {
     browserSync.init({
         proxy: 'http://opencart.me'
     });
-    gulp.watch('catalog/view/theme/'+THEME_NAME+'/dev/styles/main.scss', ['styles']);
-    gulp.watch('catalog/view/theme/'+THEME_NAME+'/dev/styles/**/*.scss', ['styles']);
-    gulp.watch('catalog/view/theme/'+THEME_NAME+'/template/**/*.tpl', browserSync.reload);
-    gulp.watch('catalog/view/theme/'+THEME_NAME+'/dev/scripts/main.js', ['scripts']);
-    gulp.watch('catalog/view/theme/'+THEME_NAME+'/libs/**/*', browserSync.reload);
+    gulp.watch(settings.input.styles(), ['styles']);
+    gulp.watch(settings.input.scripts(), ['scripts']);
+    gulp.watch(settings.input.reload(), browserSync.reload);
 });
 
 // ### JSHint
 // `gulp jshint` - Lints configuration JSON and project JS.
 gulp.task('jshint', function () {
-    return gulp.src('./catalog/view/theme/'+THEME_NAME+'/dev/scripts/main.js')
+    return gulp.src(settings.input.scripts())
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(gulpif(enabled.failJSHint, jshint.reporter('fail')));
@@ -62,20 +89,24 @@ gulp.task('jshint', function () {
 // `gulp scripts` - Runs JSHint then compiles, combines, and optimizes Bower JS
 // and project JS.
 gulp.task('scripts', ['jshint'], function () {
-    return gulp.src('catalog/view/theme/'+THEME_NAME+'/dev/scripts/main.js')
+    return gulp.src(settings.input.scripts())
         .pipe(gulpif(enabled.maps, sourcemaps.init()))
         .pipe(concat('main.js'))
         .pipe(minify({
-            ext:{
-                src:'-debug.js',
-                min:'.js'
-            },
+            ext: {
+                src: '-debug.js',
+                min: '.js'
+            }
         }))
         .pipe(gulpif(enabled.maps, sourcemaps.write()))
-        .pipe(gulp.dest('./catalog/view/theme/'+THEME_NAME+'/scripts/'))
+        .pipe(gulp.dest(settings.output.scripts))
         .pipe(browserSync.reload({stream: true}))
 });
 
 // ### Gulp
 // `gulp` - Run a complete build. To compile for production run `gulp --production`.
-gulp.task('default', ['scripts','styles']);
+gulp.task('default', ['scripts', 'styles']);
+
+gulp.task('test', function () {
+    console.log(settings.input.styles()[0]);
+});
